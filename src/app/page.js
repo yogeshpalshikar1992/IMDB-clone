b21eb7a237ffeff0ef9b0da6217c19a6610a255e
Yogesh Palshikar <yogeshpalshikar1992@gmail.com>
2024-01-03 21:29:23 +0530

    added error page

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Results from '@/components/Results';
 import React from 'react'
 
 const API_KEY = process.env.TMBD_API_KEY
-export default async function home(searchParams) {
+export default async function home({searchParams}) {
   const genre = searchParams.genre || 'fetchTrending'
   const res = await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`);
   const data = await res.json()
