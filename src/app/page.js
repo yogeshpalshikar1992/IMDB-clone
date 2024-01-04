9524c16fbf1a211bbbfd95cc358102a1daf65284
Yogesh Palshikar <yogeshpalshikar1992@gmail.com>
2024-01-04 07:43:18 +0530

    added loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Results from '@/components/Results';
 import React from 'react'
+import { resolve } from 'styled-jsx/css';
 
 const API_KEY = process.env.TMBD_API_KEY
 export default async function home({searchParams}) {
   const genre = searchParams.genre || 'fetchTrending'
-  const res = await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`);
+  const res = await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`,{ next: { revalidate: 10000 } });
   const data = await res.json()
   // console.log(data);
   if(!res.ok){
